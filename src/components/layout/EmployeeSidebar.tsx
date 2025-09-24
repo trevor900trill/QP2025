21a1feae6064baa309af236af24faa11ec5f35fd
Trevor Mugo <trevormugolawrence@gmail.com>
2025-09-24 11:53:41 +0000

    make it less thicker too

--- src/components/layout/EmployeeSidebar.tsx
+++ src/components/layout/EmployeeSidebar.tsx
@@ -23,7 +23,7 @@ export function EmployeeSidebar() {
     const pathname = usePathname();
 
     return (
-        <Sidebar className="hidden border-r border-r-[--sidebar-border] bg-sidebar md:block">
+        <Sidebar className="hidden border-r bg-sidebar md:block">
             <SidebarHeader>
                 <Link href="/module-select" className="flex items-center gap-2">
                     <Image src="/logo.svg" alt="QwikPace Logo" width={32} height={32} />
